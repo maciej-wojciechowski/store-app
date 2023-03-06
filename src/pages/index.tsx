c69619fa2500b12a384bf18a55aba3feed972fba
Maciej Wojciechowski <maciej.wojciechowski@redcompass.com>
2023-03-06 23:13:18 +0100

    initial select - category

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,6 @@
+import { Category } from "@prisma/client";
+import { Select } from "antd";
+import { type DefaultOptionType } from "antd/es/select";
 import { type NextPage } from "next";
 import ShopItemCard from "~/components/shopItem/ShopItemCard";
 
@@ -5,16 +8,23 @@ import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
   const { data: shopItemsData } = api.shopItem.getAll.useQuery();
-
+  const getOptions: () => DefaultOptionType[] = () =>
+    Object.keys(Category as Record<string, string>).map((c) => ({
+      label: c,
+      value: c,
+    }));
   return (
-    <div className="grid grid-cols-3 gap-10">
-      {shopItemsData?.length ? (
-        shopItemsData.map((item) => (
-          <ShopItemCard key={item.id} shopItemData={item} />
-        ))
-      ) : (
-        <span>No Items</span>
-      )}
+    <div>
+      <Select className="mb-10 w-36" options={getOptions()} />
+      <div className="grid grid-cols-3 gap-10">
+        {shopItemsData?.length ? (
+          shopItemsData.map((item) => (
+            <ShopItemCard key={item.id} shopItemData={item} />
+          ))
+        ) : (
+          <span>No Items</span>
+        )}
+      </div>
     </div>
   );
 };
