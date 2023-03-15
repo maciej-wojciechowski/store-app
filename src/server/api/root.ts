cfaba59f2b3582b82755fdda8dedc04c324d2a2e
Maciej Wojciechowski <maciej.wojciechowski@redcompass.com>
2023-03-15 17:25:03 +0100

    example remove

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,4 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { exampleRouter } from "~/server/api/routers/example";
 import { userRouter } from "./routers/user";
 import { shopItemRouter } from "./routers/shopItem";
 
@@ -9,7 +8,6 @@ import { shopItemRouter } from "./routers/shopItem";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   user: userRouter,
   shopItem: shopItemRouter,
 });
