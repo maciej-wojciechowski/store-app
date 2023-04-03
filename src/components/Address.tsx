bb14e895ea31e681b828a0484c4b065869ac62f3
Maciej Wojciechowski <maciej.wojciechowski@redcompass.com>
2023-04-03 15:29:01 +0200

    address regexes

--- src/components/Address.tsx
+++ src/components/Address.tsx
@@ -6,6 +6,44 @@ import ControllerInput from "./common/ControllerInput";
 
 export type AddressForm = Omit<Address, "id" | "userId">;
 
+const checkIsValidAddressNumber = (s: string) => {
+  if (!s) {
+    return false;
+  }
+  // if contains only numbers return true
+  if (!/\D/.test(s)) {
+    return true;
+  }
+  // if is a number followed by letters
+  if (/^[0-9]+[a-zA-Z]+$/.test(s)) {
+    return true;
+  }
+  return false;
+};
+
+const houseNumberValidation = (v: string) => {
+  // check if is a number
+  if (!isNaN(Number(v))) {
+    return true;
+  }
+  // check if don't have slash - must be single address number
+  if (!v.includes("/")) {
+    if (!checkIsValidAddressNumber(v)) {
+      return "Incorrect house number";
+    }
+    return true;
+  }
+  // check if have slash - must be house number and flat number
+  const split = v.split("/");
+  if (
+    split.length !== 2 ||
+    !checkIsValidAddressNumber(String(split[0])) ||
+    !checkIsValidAddressNumber(String(split[1]))
+  ) {
+    return "House number or house number/flat number";
+  }
+};
+
 type Props = {
   control: Control<AddressForm>;
   handleSubmitPromise?: (e: BaseSyntheticEvent) => Promise<void>;
@@ -24,7 +62,10 @@ function Address({ control, handleSubmitPromise, isDirty, noBtn }: Props) {
           controllerProps={{
             control,
             name: "house_number",
-            rules: { required: "Required" },
+            rules: {
+              required: "Required",
+              validate: houseNumberValidation,
+            },
           }}
         />
         <ControllerInput
@@ -32,15 +73,28 @@ function Address({ control, handleSubmitPromise, isDirty, noBtn }: Props) {
           controllerProps={{
             control,
             name: "street",
-            rules: { required: "Required" },
+            rules: {
+              required: "Required",
+              pattern: {
+                value: /^[a-zA-Z]+$/,
+                message: "Must contain only letters",
+              },
+            },
           }}
         />
+
         <ControllerInput
           label="City"
           controllerProps={{
             control,
             name: "city",
-            rules: { required: "Required" },
+            rules: {
+              required: "Required",
+              pattern: {
+                value: /^[a-zA-Z]+$/,
+                message: "Must contain only letters",
+              },
+            },
           }}
         />
         <ControllerInput
@@ -48,7 +102,13 @@ function Address({ control, handleSubmitPromise, isDirty, noBtn }: Props) {
           controllerProps={{
             control,
             name: "zip_code",
-            rules: { required: "Required" },
+            rules: {
+              required: "Required",
+              pattern: {
+                value: /^\d{2}-\d{3}$/,
+                message: "Zip code must be in format 00-000",
+              },
+            },
           }}
         />
         <ControllerInput
@@ -56,7 +116,13 @@ function Address({ control, handleSubmitPromise, isDirty, noBtn }: Props) {
           controllerProps={{
             control,
             name: "phone",
-            rules: { required: "Required" },
+            rules: {
+              required: "Required",
+              pattern: {
+                value: /^\d{9}$/,
+                message: "Invalid phone number",
+              },
+            },
           }}
         />
         {noBtn ? null : (
