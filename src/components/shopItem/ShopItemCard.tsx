e1822249c74184e9fec2c61a06e834bd119d93db
Maciej Wojciechowski <maciej.wojciechowski@redcompass.com>
2023-04-04 12:24:03 +0200

    badge

--- src/components/shopItem/ShopItemCard.tsx
+++ src/components/shopItem/ShopItemCard.tsx
@@ -1,5 +1,5 @@
 import { type ShopItem } from "@prisma/client";
-import { Card } from "antd";
+import { Badge, Card } from "antd";
 import Link from "next/link";
 import React from "react";
 
@@ -10,28 +10,34 @@ type Props = {
 function ShopItemCard({ shopItemData }: Props) {
   return (
     <Link href={`item/${shopItemData.id}`}>
-      <Card
-        hoverable
-        className="shadow-xl transition-transform hover:cursor-pointer"
-        cover={
-          <div className="h-72 overflow-hidden">
-            <img
-              className="mx-auto"
-              alt={shopItemData.name}
-              src={
-                shopItemData.images.length
-                  ? shopItemData.images[0]
-                  : "/placeholder.jpeg"
-              }
-            />
-          </div>
-        }
+      <Badge.Ribbon
+        className={`${shopItemData.stock <= 0 ? "" : "hidden"}`}
+        color="red"
+        text="Out of stock"
       >
-        <Card.Meta
-          title={shopItemData.name}
-          description={"Price: " + String(shopItemData.price) + " PLN"}
-        />
-      </Card>
+        <Card
+          hoverable
+          className="shadow-xl transition-transform hover:cursor-pointer"
+          cover={
+            <div className="h-72 overflow-hidden">
+              <img
+                className="mx-auto"
+                alt={shopItemData.name}
+                src={
+                  shopItemData.images.length
+                    ? shopItemData.images[0]
+                    : "/placeholder.jpeg"
+                }
+              />
+            </div>
+          }
+        >
+          <Card.Meta
+            title={shopItemData.name}
+            description={"Price: " + String(shopItemData.price) + " PLN"}
+          />
+        </Card>
+      </Badge.Ribbon>
     </Link>
   );
 }
